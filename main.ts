1e14f8d40c0a1f97f5dc5c05050f7864044563a1
joshua122c <120193653+joshua122c@users.noreply.github.com>
2024-06-30 01:36:09 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,61 @@
 input.onButtonPressed(Button.A, function () {
-    max7219_matrix.scrollText(
-    convertToText(scr[0]),
-    75,
-    500
+    max7219_matrix.displayTextAlignRight(
+    convertToText(target_x),
+    true
     )
 })
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index <= 7; index++) {
-        max7219_matrix.displayLEDsForOne(
+    target_x = Math.floor(taeget[ball_num] / 10)
+    target_y = taeget[ball_num] % 10
+    if (ball_x > target_x && ball_y > target_y) {
+        max7219_matrix.setValueInMatrix(
         scr,
-        0
-        )
-        basic.pause(Ti)
-        max7219_matrix.toogleValueInMatrix(
-        scr,
-        ball[0],
-        ball[1]
+        ball_x,
+        ball_y,
+        1
         )
         max7219_matrix.displayLEDsForOne(
         scr,
         0
         )
         basic.pause(Ti)
-        ball[0] = 7 - 6 + index
-        ball[1] = 7 - 6 + index
+        if (ball_x > target_x) {
+            max7219_matrix.toogleValueInMatrix(
+            scr,
+            ball_x,
+            ball_y
+            )
+            max7219_matrix.displayLEDsForOne(
+            scr,
+            0
+            )
+            basic.pause(Ti)
+            ball_x += -1
+            ball_y += -1
+        }
     }
+    if (ball_x * 10 + ball_y != taeget[ball_num]) {
+    	
+    }
+    max7219_matrix.setValueInMatrix(
+    scr,
+    target_x,
+    target_y,
+    1
+    )
+    max7219_matrix.displayLEDsForOne(
+    scr,
+    0
+    )
 })
-let Ti = 0
-let ball: number[] = []
+let target_y = 0
+let target_x = 0
 let scr: number[][] = []
+let ball_num = 0
+let Ti = 0
+let taeget: number[] = []
+let ball_y = 0
+let ball_x = 0
 max7219_matrix.setup(
 1,
 DigitalPin.P16,
@@ -37,16 +64,24 @@ DigitalPin.P14,
 DigitalPin.P13
 )
 max7219_matrix.clearAll()
+ball_x = 7
+ball_y = 7
+taeget = [
+0,
+1,
+10,
+11,
+2
+]
+Ti = 150
+ball_num = 0
 scr = max7219_matrix.getEmptyMatrix()
-ball = [7, 7]
-let ball_num = 0
-max7219_matrix.setValueInMatrix(
+target_x = Math.floor(taeget[ball_num] / 10)
+target_y = taeget[ball_num] % 10
+max7219_matrix.displayLEDsForOne(
 scr,
-ball[0],
-ball[1],
-1
+0
 )
-Ti = 50
 basic.forever(function () {
 	
 })
